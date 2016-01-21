802b3d72477c0547ee51c848470d1cc0aa50beda
Ninib83 <Ninib_83@hotmail.com>
2016-01-21 11:30:53 +0100

    new

--- src/NewFamilyTree/Startup.cs
+++ src/NewFamilyTree/Startup.cs
@@ -50,7 +50,7 @@ namespace NewFamilyTree
 
             services.AddMvc();
 
-            // Add application services.
+
             services.AddTransient<IEmailSender, AuthMessageSender>();
             services.AddTransient<ISmsSender, AuthMessageSender>();
         }
